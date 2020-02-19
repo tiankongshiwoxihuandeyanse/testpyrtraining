7b5f84d1a808f2fe1e95d94980c5d3e3fce2c065
wangqun <1164843195@qq.com>
2020-02-19 16:57:02 +0800

    新增加一个test.txt文件，test.py文件中删除了4个print，留下2个print

--- api_auto/case/shop/test.py
+++ api_auto/case/shop/test.py
@@ -2,7 +2,3 @@ import os
 
 print(os.path.abspath(__file__))
 print(os.path.dirname(__file__))
-print(os.path.abspath(__file__))
-print(os.path.dirname(__file__))
-print(os.path.abspath(__file__))
-print(os.path.dirname(__file__))
